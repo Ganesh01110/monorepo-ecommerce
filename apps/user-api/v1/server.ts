d404aa008389bd1806bd6cb35b7be7f6e66b06d7
Ganesh sahu <sahug8279@gmail.com>
2024-06-24 21:54:40 +0530

    add server.ts

--- apps/user-api/v1/server.ts
+++ apps/user-api/v1/server.ts
@@ -1,22 +1,63 @@
-import app from "./src/app";
-import {config} from "./src/config/config"
-import connectdb from "./src/config/db"
+const app = require("./app");
+const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
 
-const startServer = async ()=>{
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
 
-    //database connection
-    await connectdb();
-
-
-    // const port = process.env.PORT || 3000;
-     const port = config.port || 3000;
-
-
-    app.listen(port,()=>{
-        console.log(`listening on port : ${port}`);
-    });
-    // port with the callback it starts when the metod is being called
-    // it also indicates the port is also started running
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-startServer();
+// Connecting to database
+connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+
+
+// import app from "./src/app";
+// import {config} from "./src/config/config"
+// import connectdb from "./src/config/db"
+
+// const startServer = async ()=>{
+
+//     //database connection
+//     await connectdb();
+
+
+//     // const port = process.env.PORT || 3000;
+//      const port = config.port || 3000;
+
+
+//     app.listen(port,()=>{
+//         console.log(`listening on port : ${port}`);
+//     });
+//     // port with the callback it starts when the metod is being called
+//     // it also indicates the port is also started running
+// }
+
+// startServer();
