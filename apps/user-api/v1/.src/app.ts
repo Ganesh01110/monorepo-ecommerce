c647b7a3a51f12bed699183007d0dc0a7c756570
Ganesh sahu <sahug8279@gmail.com>
2024-07-05 23:52:09 +0530

    modify build file path

--- apps/user-api/v1/.src/app.ts
+++ apps/user-api/v1/.src/app.ts
@@ -31,10 +31,10 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../v1/build")));
 
 app.get("*", (req:Request, res:Response) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../v1/build/index.html"));
 });
 
 // Middleware for Errors
