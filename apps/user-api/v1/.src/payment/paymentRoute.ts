76e75b88444278bb85d7f813a698e09a74b4221a
Ganesh sahu <sahug8279@gmail.com>
2024-07-06 00:25:36 +0530

    corrected export format

--- apps/user-api/v1/.src/payment/paymentRoute.ts
+++ apps/user-api/v1/.src/payment/paymentRoute.ts
@@ -8,4 +8,5 @@ router.route("/payment/process").post(isAuthenticatedUser, processPayment);
 
 router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
 
-module.exports = router;
+// module.exports = router;
+export default router;
